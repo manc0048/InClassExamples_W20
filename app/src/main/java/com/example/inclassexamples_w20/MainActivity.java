b7a1fab06b997e216f8c25b68df1771eb1f63734
Eric Torunski <etorunski@gmail.com>
2020-02-19 12:31:19 -0500

    Added week 5 database examples

--- app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_w20/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.inclassexamples_w20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute("http://torunski.ca/CST2335.xml");  //Type 1
+    }
+                                                //Type1     Type2   Type3
+    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
+    {
+              //Type3                Type1
+        public String doInBackground(String ... args)
+        {
+            return "Done";
+        }
+
+                                    //Type 2
+        public void onProgressUpdate(Integer ... args)
+        {
+
+        }
+                                //Type3
+        public void onPostExecute(String fromDoInBackground)
+        {
+            Log.i("HTTP", fromDoInBackground);
+        }
     }
 }
